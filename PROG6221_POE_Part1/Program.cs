e5eb1d72547c87b832a384613acbfe738100ef76
Alexi <Alexi@DESKTOP-Q66LT49>
2023-04-26 10:37:42 +0200

    Add project files.

---
+++ PROG6221_POE_Part1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG6221_POE_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
