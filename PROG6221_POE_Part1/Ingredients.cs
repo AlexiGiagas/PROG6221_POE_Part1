364ecd173f6d9e6a267c6c4237d9381bdf29e0b2
alexi giagas <alexi.giagas@gmail.com>
2023-04-27 10:42:19 +0200

    trying a different approch because i got stuck with the previous one

--- PROG6221_POE_Part1/Ingredients.cs
+++ PROG6221_POE_Part1/Ingredients.cs
@@ -8,52 +8,15 @@ namespace PROG6221_POE_Part1
 {
     class Ingredients
     {
-        public static int[] numOfIngredients = new int[1];
-        public static string[] nameOfIngredients = new string[numOfIngredients[0]];
-        public static int[] quantOfIngredients = new int[numOfIngredients[0]];
-        public static string[] unitOfIngredients = new string[numOfIngredients[0]];
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
 
-        public Ingredients()
+        public Ingredients(string name, double quantity, string unit)
         {
-            int[] numOfIngredients = new int[1];
-
-            Console.WriteLine("Please enter the number of ingredients:");
-
-            for (int i = 0; i < numOfIngredients.Length; i++)
-            {
-                numOfIngredients[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            string[] nameOfIngredients = new string[numOfIngredients[0]];
-
-            Console.WriteLine("Please enter " + numOfIngredients[0] + " ingredients:");
-
-            for (int i = 0; i < nameOfIngredients.Length; i++)
-            {
-                nameOfIngredients[i] = Console.ReadLine();
-            }
-
-            Console.WriteLine("Please enter the quantity for each ingredient:");
-
-            int[] quantOfIngredients = new int[numOfIngredients[0]];
-
-            for (int i = 0; i < quantOfIngredients.Length; i++)
-            {
-                quantOfIngredients[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Console.WriteLine("Please enter the unit of measurement for each ingredient:");
-
-            string[] unitOfIngredients = new string[numOfIngredients[0]];
-
-            for (int i = 0; i < unitOfIngredients.Length; i++)
-            {
-                unitOfIngredients[i] = Console.ReadLine();
-            }
-
-            Console.Clear();
-
-            Menu menu = new Menu();
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
         }
     }
 }
--- PROG6221_POE_Part1/Program.cs
+++ PROG6221_POE_Part1/Program.cs
@@ -6,10 +6,110 @@ namespace PROG6221_POE_Part1
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-            Menu menu = new Menu();
+            Recipe recipe = new Recipe();
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter 'new' to start a new recipe, 'scale' to scale the recipe, 'reset' to reset the scaling, or 'exit' to quit.");
+                string input = Console.ReadLine();
+
+                if (input == "new")
+                {
+                    recipe = CreateRecipe();
+                    DisplayRecipe(recipe);
+                }
+                else if (input == "scale")
+                {
+                    ScaleRecipe(recipe);
+                    DisplayRecipe(recipe);
+                }
+                else if (input == "reset")
+                {
+                    ResetScaling(recipe);
+                    DisplayRecipe(recipe);
+                }
+                else if (input == "exit")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+        }
+
+        static Recipe CreateRecipe()
+        {
+            Recipe recipe = new Recipe();
+
+            Console.WriteLine("Enter the number of ingredients:");
+            int numIngredients = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine($"\nIngredient {i + 1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Quantity: ");
+                double quantity = double.Parse(Console.ReadLine());
+                Console.Write("Unit of measurement: ");
+                string unit = Console.ReadLine();
+
+                recipe.Ingredients.Add(new Ingredients(name, quantity, unit));
+            }
+
+            Console.WriteLine("\nEnter the number of steps:");
+            int numSteps = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numSteps; i++)
+            {
+                Console.WriteLine($"\nStep {i + 1}:");
+                string description = Console.ReadLine();
+                recipe.Steps.Add(description);
+            }
+            Console.Clear();
+            return recipe;
+        }
+
+        static void DisplayRecipe(Recipe recipe)
+        {
+            Console.WriteLine("\nRECIPE\n");
+
+            foreach (Ingredients ingredient in recipe.Ingredients)
+            {
+                Console.WriteLine($"{ingredient.Quantity} {ingredient.Unit} {ingredient.Name}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < recipe.Steps.Count; i++)
+            {
+                Console.WriteLine($"Step {i + 1}: {recipe.Steps[i]}");
+            }
+        }
+
+        static void ScaleRecipe(Recipe recipe)
+        {
+            Console.WriteLine("\nEnter the scaling factor (0.5, 2, or 3):");
+            double factor = double.Parse(Console.ReadLine());
+
+            foreach (Ingredients ingredient in recipe.Ingredients)
+            {
+                ingredient.Quantity *= factor;
+            }
+        }
+
+        static void ResetScaling(Recipe recipe)
+        {
+            foreach (Ingredients ingredient in recipe.Ingredients)
+            {
+                ingredient.Quantity /= recipe.ScaleFactor;
+            }
+
+            recipe.ScaleFactor = 1;
         }
     }
 }
