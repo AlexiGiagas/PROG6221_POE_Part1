9c25513201455b3ff1aba82114d74a1c55ed4964
alexi giagas <alexi.giagas@gmail.com>
2023-04-27 10:43:05 +0200

    updated ingredients class

--- PROG6221_POE_Part1/Program.cs
+++ PROG6221_POE_Part1/Program.cs
@@ -43,73 +43,22 @@ namespace PROG6221_POE_Part1
 
         static Recipe CreateRecipe()
         {
-            Recipe recipe = new Recipe();
-
-            Console.WriteLine("Enter the number of ingredients:");
-            int numIngredients = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < numIngredients; i++)
-            {
-                Console.WriteLine($"\nIngredient {i + 1}:");
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                Console.Write("Quantity: ");
-                double quantity = double.Parse(Console.ReadLine());
-                Console.Write("Unit of measurement: ");
-                string unit = Console.ReadLine();
-
-                recipe.Ingredients.Add(new Ingredients(name, quantity, unit));
-            }
-
-            Console.WriteLine("\nEnter the number of steps:");
-            int numSteps = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < numSteps; i++)
-            {
-                Console.WriteLine($"\nStep {i + 1}:");
-                string description = Console.ReadLine();
-                recipe.Steps.Add(description);
-            }
-            Console.Clear();
-            return recipe;
+            
         }
 
         static void DisplayRecipe(Recipe recipe)
         {
-            Console.WriteLine("\nRECIPE\n");
-
-            foreach (Ingredients ingredient in recipe.Ingredients)
-            {
-                Console.WriteLine($"{ingredient.Quantity} {ingredient.Unit} {ingredient.Name}");
-            }
-
-            Console.WriteLine();
-
-            for (int i = 0; i < recipe.Steps.Count; i++)
-            {
-                Console.WriteLine($"Step {i + 1}: {recipe.Steps[i]}");
-            }
+            
         }
 
         static void ScaleRecipe(Recipe recipe)
         {
-            Console.WriteLine("\nEnter the scaling factor (0.5, 2, or 3):");
-            double factor = double.Parse(Console.ReadLine());
-
-            foreach (Ingredients ingredient in recipe.Ingredients)
-            {
-                ingredient.Quantity *= factor;
-            }
+            
         }
 
         static void ResetScaling(Recipe recipe)
         {
-            foreach (Ingredients ingredient in recipe.Ingredients)
-            {
-                ingredient.Quantity /= recipe.ScaleFactor;
-            }
-
-            recipe.ScaleFactor = 1;
+            
         }
     }
 }
--- PROG6221_POE_Part1/Recipe.cs
+++ PROG6221_POE_Part1/Recipe.cs
@@ -11,16 +11,7 @@ namespace PROG6221_POE_Part1
 {
     class Recipe
     {
-        public List<Ingredients> Ingredients { get; set; }
-        public List<string> Steps { get; set; }
-        public double ScaleFactor { get; set; }
-
-        public Recipe()
-        {
-            Ingredients = new List<Ingredients>();
-            Steps = new List<string>();
-            ScaleFactor = 1;
-        }
+        
 
     }
 }
